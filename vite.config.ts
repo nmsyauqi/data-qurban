bfa91d50b5fa5b2b5a7831e36c1dca93c2cf9390
nmsyauqi <syauqimuhammad430@gmail.com>
2026-05-27 12:32:05 +0700

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/data-qurban/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
